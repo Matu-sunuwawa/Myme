28114c7c4db5bcd0d993ac2e64079c9c8660c5db
Matyas Sina <matyassinaadugna@gmail.com>
2023-03-16 06:51:37 -0700

    README changed

--- Myme/portfolio/views.py
+++ Myme/portfolio/views.py
@@ -1,7 +1,7 @@
 # from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
-from django.contrib.auth.models import Portfolio, auth
+# from django.contrib.auth.models import Portfolio, auth
 from django.contrib import messages
 # from django.contrib.auth.models import UserManager
 # from django.core.exceptions import FieldDoesNotExist
